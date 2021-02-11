60f65ea937219b7867151e0a6086edc3c716c467
Mark <SwiftTactics@gmail.com>
2021-02-11 15:43:11 -0500

    added find repetitions

--- src/NodeTest/Demo.java
+++ src/NodeTest/Demo.java
@@ -1,17 +1,56 @@
 package NodeTest;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo {
     public static void main(String[] args) {
-        SingleLL ll = new SingleLL();
+//        SingleLL ll = new SingleLL();
+//
+//        ll.addNode(1);
+//        ll.addNode(2);
+//        ll.addNode(3);
+//        ll.addNode(4);
+//        ll.printList();
+//        ll.reverseList();
+//        System.out.println();
+//        ll.printList();
 
-        ll.addNode(1);
-        ll.addNode(2);
-        ll.addNode(3);
-        ll.addNode(4);
-        ll.printList();
-        ll.reverseList();
-        System.out.println();
-        ll.printList();
+        int[] array = {4,4,4,5,6,6,6,1,1,4,4,4,4,5};
 
+        System.out.println(findRepeated(array));
     }
-}
+
+        // 1- given an array of integers find the most repeated number
+//	findRepeated([4,4,4,5,6,6,6,1,1,4,4,4,4,5]) = 4
+
+    public static int findRepeated(int[] array){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++){
+            if (!map.containsKey(array[i])){
+                map.put(array[i], 1);
+            } else {
+                map.put(array[i], map.get(array[i]) + 1);
+            }
+        }
+
+        int currentNum = 0;
+        int currentMax = 0;
+
+        for (Map.Entry<Integer, Integer> num: map.entrySet()){
+            if (num.getValue() > currentMax){
+                currentNum = num.getKey();
+                currentMax = num.getValue();
+            }
+        }
+        return currentNum;
+    }
+
+
+// 2- From a unsorted array, check whether there are any two numbers that will sum up to a given number
+//	sumFinder([6,4,3,2,1,7], 9) = true
+//	sumFinder([6,4,3,2,1,7], 2) = false}
+    }
