d9e5b1375f3a43ef05e458672580b0a012cee4f7
Mark <SwiftTactics@gmail.com>
2021-02-11 16:01:15 -0500

    sumFinder added

--- src/NodeTest/Demo.java
+++ src/NodeTest/Demo.java
@@ -1,6 +1,8 @@
 package NodeTest;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,8 +20,10 @@ public class Demo {
 //        ll.printList();
 
         int[] array = {4,4,4,5,6,6,6,1,1,4,4,4,4,5};
+        int[] arr2 = {6,4,3,2,1,7};
 
         System.out.println(findRepeated(array));
+        System.out.println(sumFinder(arr2, 9));
     }
 
         // 1- given an array of integers find the most repeated number
@@ -53,4 +57,22 @@ public class Demo {
 // 2- From a unsorted array, check whether there are any two numbers that will sum up to a given number
 //	sumFinder([6,4,3,2,1,7], 9) = true
 //	sumFinder([6,4,3,2,1,7], 2) = false}
+
+    public static boolean sumFinder(int[] array, int value){
+        Arrays.sort(array);
+
+        int leftPoint = 0;
+        int rightPoint = array.length - 1;
+
+        while (leftPoint < rightPoint){
+            if (array[leftPoint] + array[rightPoint] > value) {
+                rightPoint--;
+            } else if (array[leftPoint] + array[rightPoint] < value){
+                leftPoint++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
     }
