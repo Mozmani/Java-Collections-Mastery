d91be24d9ff9a3b4723b0663804baf4c49b45fb3
Mark <SwiftTactics@gmail.com>
2021-03-20 17:31:43 -0400

    adjusted basket to use a linkedhashmap

--- src/StockList/StockList.java
+++ src/StockList/StockList.java
@@ -2,13 +2,14 @@ package StockList;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StockList {
     private final Map<String, StockItem> list;
 
     public StockList() {
-        this.list = new HashMap<>();
+        this.list = new LinkedHashMap<>();
     }
     public int addStock(StockItem item){
         if(item != null){
